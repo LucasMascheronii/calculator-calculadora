da1f305882134613ead68ecc0918bd8661ffe4b6
LucasMascheronii <141967645+LucasMascheronii@users.noreply.github.com>
2023-09-30 18:37:09 -0300

    Add files via upload

---
+++ import pymath.py
@@ -0,0 +1,19 @@
+value1= input("please select your first value: ")
+operation= input("please select your operation: + - * / : ")
+value2= input("please select your second value: ") 
+
+if operation== "+":
+  result= int(value1)+int(value2)
+  print (result)
+
+if operation== "-":
+  result= int(value1)-int(value2)
+  print (result)
+
+if operation=="*":
+  result= int(value1)*int(value2)
+  print (result)
+
+if operation== "/":
+  result= int(value1)/int(value2)  
+  print(result)
